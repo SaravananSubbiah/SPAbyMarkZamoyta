742f61db0f4b99293a5380fcb3424c7c221880c0
Saravanan Saravanan <Saravanan Saravanan>
2017-04-29 00:17:20 -0400

    image name changed

--- src/app/services/app-data.service.ts
+++ src/app/services/app-data.service.ts
@@ -12,7 +12,7 @@ export class AppDataService {
     { id: 2, name:"Luxembourg",   epiIndex: 83.29 },
     { id: 3, name:"Australia", epiIndex: 82.4 },
     { id: 4, name:"Singapore", epiIndex: 81.78 },
-    { id: 5, name:"Czech Republic", epiIndex: 81.47 },
+    { id: 5, name:"Czech-Republic", epiIndex: 81.47 },
     { id: 6, name:"Germany", epiIndex: 80.47 },
     { id: 7, name:"Spain", epiIndex: 79.09 },
     { id: 8, name:"Austria", epiIndex: 78.32 },
